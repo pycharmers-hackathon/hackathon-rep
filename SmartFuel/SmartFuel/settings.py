231e1e0f7dd92055c9f365ce4e34dde9d18a5709
Thodoris Sotiropoulos <theosotr@windowslive.com>
2015-07-18 17:34:04 +0300

    Add files

--- SmartFuel/SmartFuel/settings.py
+++ SmartFuel/SmartFuel/settings.py
@@ -65,6 +65,10 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    },
+    'OPTIONS': {
+        'charset': 'UTF-8',
+        'use_unicode': True
     }
 }
 
--- SmartFuel/SmartFuel/views.py
+++ SmartFuel/SmartFuel/views.py
@@ -1,3 +1,5 @@
+# coding=utf-8
+
 from django.http import *
 from django.shortcuts import render_to_response
 from django.template import Template, Context
@@ -7,8 +9,26 @@ from django.contrib import messages
 from django.core import serializers
 from django.core.context_processors import csrf
 from django.shortcuts import render
-from data.models import *
+
+import csv
+import io, sys
+from model import PatrolStation
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
 
 @csrf_exempt
 def index(request, page=None):
+    with io.open('c:/users/theo/desktop/PratiriaPouApostellounStoixeiaGGPS.csv', 'r') as csv_file:
+            reader = csv.reader(csv_file, delimiter=';')
+            counter = 0
+            for row in reader:
+                row = [x.decode('utf-8') for x in row]
+            r = PatrolStation(row[0], row[1], row[2], row[3], row[4], row[5],
+                              row[6], row[7], 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0)
+            r.save()
     return render(request, 'index.html')
+
+def nearest_patrol_stations(request):
+    latitute = request.get('lat')
+    longitute = request.get('lng')
